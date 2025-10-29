Fix R2 silent error logging, empty dir error, UID api and dormant miner incentive errors

distributed_training/base/neuron.py
@@ -36,8 +36,10 @@ from distributed_training.utils.config import (
     R2Access,
     R2Config,
 )
+from distributed_training.utils.logger import setup_logging
 from distributed_training.utils.misc import ttl_get_block
 from dotenv import load_dotenv
+
 
 import torch, torch.distributed as dist
 import datetime as dt
@@ -167,6 +169,9 @@ class BaseNeuron(ABC):
         dist.broadcast(master_uid, src=0, group=self.gloo_group)
         self.master_uid = master_uid[0].item()
 
+        # Setup Logging
+        setup_logging(self, config=self.config)
+
         # Create the R2 data model
         r2 = R2Config(
             bucket_name=f"{self.config.neuron.global_model_name.split('/')[-1]}-{self.uid:03d}"
@@ -186,6 +191,11 @@ class BaseNeuron(ABC):
 
         # Save directory
         self.output_dir = os.path.join(os.getcwd(), self.config.r2.bucket_name)
+        os.makedirs(self.output_dir, exist_ok=True)
+        os.makedirs(
+            os.path.join(os.getcwd(), self.config.neuron.global_model_name),
+            exist_ok=True,
+        )
 
         # Init Step
         self.step = 0
@@ -258,10 +268,12 @@ class BaseNeuron(ABC):
         self.reload_state_event = threading.Event()
 
     # @abstractmethod # miner is not using this anymore
-    async def forward(self, synapse: bt.Synapse) -> bt.Synapse: ...
+    async def forward(self, synapse: bt.Synapse) -> bt.Synapse:
+        ...
 
     @abstractmethod
-    def run(self): ...
+    def run(self):
+        ...
 
     def sync(self):
         """

distributed_training/utils/compression.py
@@ -147,7 +147,8 @@ class CompressDCT(Generic[Q]):
         use_quantization: Literal[True] = True,
         quantization_bins: int = 256,
         quantization_range: int = 6,
-    ) -> None: ...
+    ) -> None:
+        ...
 
     @overload
     def __init__(
@@ -156,7 +157,8 @@ class CompressDCT(Generic[Q]):
         use_quantization: Literal[False] = False,
         quantization_bins: int = 256,
         quantization_range: int = 6,
-    ) -> None: ...
+    ) -> None:
+        ...
 
     @torch.no_grad()
     def __init__(
@@ -188,14 +190,16 @@ class CompressDCT(Generic[Q]):
         self: "CompressDCT[Literal[True]]",
         x: torch.Tensor,
         topk: int,
-    ) -> tuple[IdxT, ValT, ShapeT, TotK, QuantParamsT]: ...
+    ) -> tuple[IdxT, ValT, ShapeT, TotK, QuantParamsT]:
+        ...
 
     @overload
     def compress(
         self: "CompressDCT[Literal[False]]",
         x: torch.Tensor,
         topk: int,
-    ) -> tuple[IdxT, ValT, ShapeT, TotK]: ...
+    ) -> tuple[IdxT, ValT, ShapeT, TotK]:
+        ...
 
     @torch.no_grad()
     def compress(self, x: torch.Tensor, topk: int, quantize: bool = False):  # type: ignore[override]

neurons/miner.py
@@ -261,6 +261,7 @@ class Miner(BaseMinerNeuron):
                 )
             )
         elif not self.all_reduce_success_status:
+            block_list = self.model.config.block_list
             if self.local_progress.epoch > self.global_progress.epoch:
                 self.logger.info(
                     f"Local Epoch {self.local_progress.epoch} Ahead Of Global Epoch {self.global_progress.epoch}. Loading Latest Model State."
@@ -275,7 +276,7 @@ class Miner(BaseMinerNeuron):
                     uid=self.uid,
                     epoch=self.global_progress.epoch,
                 )
-            self.model.config.block_list = []
+            self.model.config.block_list = block_list
             self.resume_training()
             self.all_reduce_success_status = True
         else:
@@ -446,7 +447,9 @@ class Miner(BaseMinerNeuron):
                             self.logger.info(
                                 "Cancelling Ongoing Model Upload For AllReduce Operation"
                             )
-                            self.model.config.block_list = block_list + self.model.config.block_list
+                            self.model.config.block_list = (
+                                block_list + self.model.config.block_list
+                            )
                             return False
                         else:
                             time.sleep(5)
@@ -472,7 +475,9 @@ class Miner(BaseMinerNeuron):
                         "Maximum retry limit reached. Unable to upload state to HF Hub."
                     )
                     if self.master:
-                        self.model.config.block_list = block_list + self.model.config.block_list
+                        self.model.config.block_list = (
+                            block_list + self.model.config.block_list
+                        )
                     raise
 
         return False
@@ -576,7 +581,7 @@ class Miner(BaseMinerNeuron):
 
                 pages = await DatasetLoader.next_pages(
                     offset=self.current_block,
-                    n_pages=35,
+                    n_pages=5,
                     seed=self.uid + self.local_rank,
                 )
                 rng = np.random.default_rng(hash(self.uid) & 0xFFFFFFFF)